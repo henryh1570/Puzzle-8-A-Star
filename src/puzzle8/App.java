84ad3efa4cc9ef9b4c613cb6630a5bed68ebbae0
henryh1570 <henryhu@cpp.edu>
2017-04-24 00:35:14 -0700

    implementing solve

--- src/puzzle8/App.java
+++ src/puzzle8/App.java
@@ -8,13 +8,14 @@ public class App {
 
 	public static Scanner k = new Scanner(System.in);
 	public static ArrayList<Character> list = new ArrayList<Character>();
+	public static final String GOAL_STATE = "012345678";
 	
 	public static void main(String[] args) {
 		// Preemptively prepare random puzzle generation
-		for (char c: "012345678".toCharArray()) {
+		for (char c: GOAL_STATE.toCharArray()) {
 			list.add(c);
 		}
-		
+
 		int choice = -1;
 		while (choice != 3) {
 			promptPuzzle();
@@ -39,10 +40,21 @@ public class App {
 	// Do some operation given user-input
 	public static void performOperations(int choice) {
 		String puzzle = "";
+		int input = -1;
 		switch (choice) {
 		case 1:
 			puzzle = getRandomPuzzle();
 			System.out.println("Random Puzzle: " + puzzle);
+			promptSolution();
+			while (input == -1) {
+				input = getInput();
+			}
+			
+			if (input == 1) { // Heuristic 1
+				
+			} else {
+				
+			}
 			// TODO: Solving Stuff
 			break;
 		case 2:
@@ -52,6 +64,16 @@ public class App {
 			if (!isSolvable(puzzle)) {
 				System.out.println("Invalid puzzle");
 			} else {
+				promptSolution();
+				while (input == -1) {
+					input = getInput();
+				}
+				
+				if (input == 1) { // Heuristic 1
+					
+				} else {
+					
+				}
 				//TODO: Solving stuff
 			}			
 			break;
@@ -112,6 +134,61 @@ public class App {
 		}
 		
 		return true;
+	}
+	
+	// h(n) = # of misplaced tiles
+	public static int heuristicOne(String state) {
+		char[] goal = GOAL_STATE.toCharArray();
+		char[] current = state.toCharArray();
+		
+		int matches = 0;
+		
+		for (int i = 0; i < goal.length; i++) {
+			if (goal[i] == current[i]) {
+				matches++;
+			}
+		}
+		
+		return matches;
+	}
+	
+	// h(n) =  sum of distances of tiles from their goal positions
+	public static int heuristicTwo(String state) {
+		char[] current = state.toCharArray();
+		
+		int sumOfMoves = 0;
+		
+		for (int i = 0; i < current.length; i++) {
+			sumOfMoves += movesFromSolution(i, Integer.parseInt(""+ current[i]));
+		}
+		
+		return sumOfMoves;
+	}
+	
+	//TODO: 
+	public static ArrayList<String> solve(String initialState) {
+		ArrayList<String> path = new ArrayList<String>();
+		String currentState = initialState;
+		while (!currentState.equals(GOAL_STATE)) {
+			
+		}
+		return path;
+	}
+	
+	public static int movesFromSolution(int currentPosition, int value) {
+		// Coordinates of value's index in a 3 x 3 grid
+		// X and Y are the offset of movements from the goal position
+		int goalPosition = GOAL_STATE.indexOf(""+value);
+		
+		int x = Math.abs((currentPosition % 3) - (goalPosition % 3));
+		int y = Math.abs((currentPosition / 3) - (goalPosition / 3));
+		
+		return x + y;
+	}
+	
+	public static void promptSolution() {
+		System.out.println("(1) Solve via heuristic 1 (# of misplaced tiles)");
+		System.out.println("(2) Solve via heuristic 2 (sum of distances of tiles from goal positions)");
 	}
 	
 	public static void promptPuzzle() {
